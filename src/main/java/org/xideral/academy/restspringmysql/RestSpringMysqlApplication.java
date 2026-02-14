1a028e7e28f64c5070fa95929025c6d56a09ef31
Edson733 <20213tn034@utez.edu.mx>
2026-02-14 09:31:38 -0600

    Creacion de proyecto Spring REST y configuracion de las properties

---
+++ src/main/java/org/xideral/academy/restspringmysql/RestSpringMysqlApplication.java
@@ -0,0 +1,13 @@
+package org.xideral.academy.restspringmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestSpringMysqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/xideral/academy/restspringmysql/RestSpringMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xideral.academy.restspringmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringMysqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
